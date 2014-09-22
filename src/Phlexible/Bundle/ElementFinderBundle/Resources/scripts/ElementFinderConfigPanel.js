e38d40e19dd00ff4814ddd64ff93430b1b3a9196
Stephan Wentz <swentz@brainbits.net>
2014-09-22 17:07:05 +0200

    element finder fixes

--- src/Phlexible/Bundle/ElementFinderBundle/Resources/scripts/ElementFinderConfigPanel.js
+++ src/Phlexible/Bundle/ElementFinderBundle/Resources/scripts/ElementFinderConfigPanel.js
@@ -72,14 +72,10 @@ Phlexible.elementfinder.ElementFinderConfigPanel = Ext.extend(Ext.Panel, {
                         value: this.values.startTreeId,
                         listeners: {
                             change: function (field) {
-                                this.getComponent(0).getComponent(0).getComponent(0).setValue(
-                                    this.getComponent(0).getComponent(0).getComponent(1).getValue()
-                                );
+                                this.getForm().findField('startTreeId').setValue(this.getForm().findField('for_tree_id').getValue());
                             },
                             load: function () {
-                                this.getComponent(0).getComponent(0).getComponent(1).setValue(
-                                    this.getComponent(0).getComponent(0).getComponent(1).getValue()
-                                );
+                                this.getForm().findField('for_tree_id').setValue(this.getForm().findField('for_tree_id').getValue());
                             },
                             scope: this
                         }
@@ -99,160 +95,154 @@ Phlexible.elementfinder.ElementFinderConfigPanel = Ext.extend(Ext.Panel, {
                         allowBlank: true
                     }]
                 },
-                    {
-                        xtype: 'fieldset',
-                        title: this.strings.sort,
-                        autoHeight: true,
-                        anchor: '-15',
-                        items: [{
-                            xtype: 'twincombobox',
-                            width: 300,
-                            listWidth: 300,
-                            fieldLabel: this.strings.sort_field,
-                            hiddenName: 'sortField',
-                            value: this.values.sortField,
-                            store: new Ext.data.JsonStore({
-                                url: Phlexible.Router.generate('elementfinder_catch_sortfields', {query: this.baseValues.elementtypeIds}),
-                                root: 'data',
-                                fields: ['ds_id', 'title', 'icon'],
-                                autoLoad: true,
-                                listeners: {
-                                    load: function() {
-                                        this.getComponent(0).getComponent(1).getComponent(0).setValue(
-                                            this.getComponent(0).getComponent(1).getComponent(0).getValue()
-                                        );
-                                    },
-                                    scope: this
-                                }
-                            }),
-                            //tpl: '<tpl for="."><div class="x-combo-list-item {icon}">{title}</div></tpl>',
-                            displayField: 'title',
-                            valueField: 'ds_id',
-                            mode: 'remote',
-                            emptyText: this.strings.unsorted,
-                            listClass: 'x-combo-list-big',
-                            editable: false,
-                            triggerAction: 'all',
-                            selectOnFocus: true,
+                {
+                    xtype: 'fieldset',
+                    title: this.strings.sort,
+                    autoHeight: true,
+                    anchor: '-15',
+                    items: [{
+                        xtype: 'twincombobox',
+                        width: 300,
+                        listWidth: 300,
+                        fieldLabel: this.strings.sort_field,
+                        hiddenName: 'sortField',
+                        value: this.values.sortField,
+                        store: new Ext.data.JsonStore({
+                            url: Phlexible.Router.generate('elementfinder_catch_sortfields', {query: this.baseValues.elementtypeIds}),
+                            root: 'data',
+                            fields: ['ds_id', 'title', 'icon'],
+                            autoLoad: true,
                             listeners: {
-                                beforequery: function (event) {
-                                    event.query = this.baseValues.elementtypeIds;
+                                load: function() {
+                                    var sortField = this.getForm().findField('sortField');
+                                    sortField.setValue(sortField.getValue());
                                 },
                                 scope: this
                             }
-                        },
-                            {
-                                xtype: 'combo',
-                                width: 300,
-                                listWidth: 300,
-                                fieldLabel: this.strings.sort_dir,
-                                hiddenName: 'sortDir',
-                                value: this.values.sortDir || 'ASC',
-                                store: new Ext.data.SimpleStore({
-                                    fields: ['title', 'value'],
-                                    data: [
-                                        [this.strings.sort_ascending, 'ASC'],
-                                        [this.strings.sort_descending, 'DESC']
-                                    ]
-                                }),
-                                displayField: 'title',
-                                valueField: 'value',
-                                mode: 'local',
-                                listClass: 'x-combo-list-big',
-                                editable: false,
-                                triggerAction: 'all',
-                                selectOnFocus: true,
-                                allowBlank: false
-                            }]
+                        }),
+                        //tpl: '<tpl for="."><div class="x-combo-list-item {icon}">{title}</div></tpl>',
+                        displayField: 'title',
+                        valueField: 'ds_id',
+                        mode: 'remote',
+                        emptyText: this.strings.unsorted,
+                        listClass: 'x-combo-list-big',
+                        editable: false,
+                        triggerAction: 'all',
+                        selectOnFocus: true,
+                        listeners: {
+                            beforequery: function (event) {
+                                event.query = this.baseValues.elementtypeIds;
+                            },
+                            scope: this
+                        }
                     },
                     {
-                        xtype: 'fieldset',
-                        title: this.strings.filter,
-                        autoHeight: true,
-                        anchor: '-15',
-                        items: [{
-                            xtype: 'twincombobox',
-                            width: 300,
-                            listWidth: 300,
-                            fieldLabel: this.strings.meta_filter,
-                            hiddenName: 'metaKey',
-                            value: this.values.metaKey,
-                            store: new Ext.data.JsonStore({
-                                url: Phlexible.Router.generate('elementfinder_catch_metakeys'),
-                                root: 'metakeys',
-                                fields: ['id', 'name'],
-                                autoLoad: true,
-                                listeners: {
-                                    load: function() {
-                                        this.getComponent(0).getComponent(2).getComponent(0).setValue(
-                                            this.getComponent(0).getComponent(2).getComponent(0).getValue()
-                                        );
-                                    },
-                                    scope: this
-                                }
-                            }),
+                        xtype: 'combo',
+                        width: 300,
+                        listWidth: 300,
+                        fieldLabel: this.strings.sort_dir,
+                        hiddenName: 'sortDir',
+                        value: this.values.sortDir || 'ASC',
+                        store: new Ext.data.SimpleStore({
+                            fields: ['title', 'value'],
+                            data: [
+                                [this.strings.sort_ascending, 'ASC'],
+                                [this.strings.sort_descending, 'DESC']
+                            ]
+                        }),
+                        displayField: 'title',
+                        valueField: 'value',
+                        mode: 'local',
+                        listClass: 'x-combo-list-big',
+                        editable: false,
+                        triggerAction: 'all',
+                        selectOnFocus: true,
+                        allowBlank: false
+                    }]
+                },
+                {
+                    xtype: 'fieldset',
+                    title: this.strings.filter,
+                    autoHeight: true,
+                    anchor: '-15',
+                    items: [{
+                        xtype: 'twincombobox',
+                        width: 300,
+                        listWidth: 300,
+                        fieldLabel: this.strings.meta_filter,
+                        hiddenName: 'metaKey',
+                        value: this.values.metaKey,
+                        store: new Ext.data.JsonStore({
+                            url: Phlexible.Router.generate('elementfinder_catch_metakeys'),
+                            root: 'metakeys',
+                            fields: ['id', 'name'],
+                            autoLoad: true,
                             listeners: {
-                                select: function (combo, record, index) {
-                                    var sortField = this.getForm().findField('metaKeywords');
-                                    sortField.store.load();
-                                },
-                                clear: function() {
-                                    this.getComponent(0).getComponent(2).getComponent(1).getStore().removeAll();
+                                load: function() {
+                                    var metaKeyField = this.getForm().findField('metaKey');
+                                    metaKeyField.setValue(metaKeyField.getValue());
                                 },
                                 scope: this
+                            }
+                        }),
+                        listeners: {
+                            select: function (combo, record, index) {
+                                var metaKeywordsFields = this.getForm().findField('metaKeywords');
+                                metaKeywordsFields.store.load();
                             },
-                            displayField: 'name',
-                            valueField: 'id',
-                            mode: 'remote',
-                            emptyText: this.strings.no_filter,
-                            listClass: 'x-combo-list-big',
-                            editable: false,
-                            triggerAction: 'all',
-                            selectOnFocus: true
+                            clear: function() {
+                                var metaKeywordsFields = this.getForm().findField('metaKeywords');
+                                metaKeywordsFields.store.removeAll();
+                            },
+                            scope: this
                         },
-                        {
-                            xtype: 'multiselect',
-                            fieldLabel: this.strings.meta_keywords,
-                            name: 'metaKeywords',
-                            value: this.values.metaKeywords,
-                            store: new Ext.data.JsonStore({
-                                url: Phlexible.Router.generate('elementfinder_catch_metakeywords', {siteroot_id: this.siteroot_id}),
-                                root: 'meta_keywords',
-                                fields: ['keyword'],
-                                listeners: {
-                                    beforeload: function (store, options) {
-                                        options['params'] = {
-                                            language: 'de',
-                                            id: ''
-                                        };
+                        displayField: 'name',
+                        valueField: 'id',
+                        mode: 'remote',
+                        emptyText: this.strings.no_filter,
+                        listClass: 'x-combo-list-big',
+                        editable: false,
+                        triggerAction: 'all',
+                        selectOnFocus: true
+                    },
+                    {
+                        xtype: 'multiselect',
+                        fieldLabel: this.strings.meta_keywords,
+                        name: 'metaKeywords',
+                        value: this.values.metaKeywords,
+                        store: new Ext.data.JsonStore({
+                            url: Phlexible.Router.generate('elementfinder_catch_metakeywords'),
+                            root: 'meta_keywords',
+                            fields: ['keyword'],
+                            autoLoad: !!this.values.metaKey,
+                            listeners: {
+                                beforeload: function (store, options) {
+                                    var metaKeyField = this.getForm().findField('metaKey');
+                                    if (!metaKeyField.isFormField || !metaKeyField.getValue()) {
+                                        return false;
+                                    }
 
-                                        if (this.getForm()) {
-                                            var keyField = this.getForm().findField('metaKey');
-                                            if (keyField && keyField.isFormField) {
-                                                options['params']['id'] = keyField.getValue();
-                                            }
-                                        }
-                                    },
-                                    load: function () {
-                                        var onLoad = this.getForm().findField('metaKeywords').setOnLoad;
-                                        if (onLoad) {
-                                            this.getForm().findField('metaKeywords').setValue(onLoad);
-                                            this.getForm().findField('metaKeywords').setOnLoad = false;
-                                        }
-                                    },
-                                    scope: this
-                                }
-                            }),
-                            width: 300,
-                            emptyText: this.strings.select_meta_keywords,
-                            valueField: 'keyword',
-                            displayField: 'keyword',
-                            tpl: '<tpl for="."><div class="ux-mselect-item' + ((Ext.isIE || Ext.isIE7) ? '" unselectable=on' : ' x-unselectable"') + '>{keyword}</div></tpl>',
-                            //                width:280,
-                            height: 165,
-                            allowBlank: true
-                        }]
+                                    options['params'] = {
+                                        language: 'de',
+                                        id: metaKeyField.getValue()
+                                    };
+                                },
+                                load: function () {
+                                    this.getForm().findField('metaKeywords').setValue(this.values.metaKeywords);
+                                },
+                                scope: this
+                            }
+                        }),
+                        width: 300,
+                        emptyText: this.strings.select_meta_keywords,
+                        valueField: 'keyword',
+                        displayField: 'keyword',
+                        tpl: '<tpl for="."><div class="ux-mselect-item' + ((Ext.isIE || Ext.isIE7) ? '" unselectable=on' : ' x-unselectable"') + '>{keyword}</div></tpl>',
+                        //                width:280,
+                        height: 165,
+                        allowBlank: true
                     }]
+                }]
             }
         ];
 
