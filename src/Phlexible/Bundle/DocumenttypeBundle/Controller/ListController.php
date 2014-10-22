eebaaedd955941ba6081158e7e89b7efde7b96a8
Stephan Wentz <stephan@wentz.it>
2014-10-22 21:01:12 +0200

    role fixes

--- src/Phlexible/Bundle/DocumenttypeBundle/Controller/ListController.php
+++ src/Phlexible/Bundle/DocumenttypeBundle/Controller/ListController.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
  *
  * @author Stephan Wentz <sw@brainbits.net>
  * @Route("/documenttypes")
- * @Security("is_granted('ROLE_DOCUMENTTYPE')")
+ * @Security("is_granted('ROLE_DOCUMENTTYPES')")
  */
 class ListController extends Controller
 {
