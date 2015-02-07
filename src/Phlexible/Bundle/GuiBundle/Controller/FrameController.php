407e28c86973049b73a14a793c5032dd86a69f69
Stephan Wentz <swentz@brainbits.net>
2015-02-07 18:54:43 +0100

    composer for gui-bundle

--- src/Phlexible/Bundle/GuiBundle/Controller/FrameController.php
+++ src/Phlexible/Bundle/GuiBundle/Controller/FrameController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route as RoutingRoute;
-use Webmozart\PathUtil\Path;
 
 /**
  * Frame controller
