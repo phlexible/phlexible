e1bdb08e34032ebd44ddc73cb718ef9d5c0e2241
Stephan Wentz <swentz@brainbits.net>
2015-04-21 13:36:15 +0200

    moved tasks to seperate repository

--- src/Phlexible/Bundle/TaskBundle/Resources/scripts/model/Comment.js
+++
@@ -1,9 +0,0 @@
-Ext.provide('Phlexible.tasks.model.Comment');
-
-Phlexible.tasks.model.Comment = Ext.data.Record.create([
-    {name: 'id'},
-    {name: 'current_state'},
-    {name: 'comment'},
-    {name: 'create_user'},
-    {name: 'create_date'}
-]);
