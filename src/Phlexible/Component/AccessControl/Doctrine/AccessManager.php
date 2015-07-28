8966a7764416637e6c33e2494106cd65c99b0bc6
Stephan Wentz <swentz@brainbits.net>
2015-07-28 17:02:41 +0200

    fix query

--- src/Phlexible/Component/AccessControl/Doctrine/AccessManager.php
+++ src/Phlexible/Component/AccessControl/Doctrine/AccessManager.php
@@ -86,7 +86,7 @@ class AccessManager implements AccessManagerInterface
             ->where($qb->expr()->eq('type', $qb->expr()->literal($objectIdentity->getType())))
             ->andWhere($qb->expr()->eq('identifier', $qb->expr()->literal($objectIdentity->getIdentifier())));
 
-        $id = $conn->fetchColumn($qb);
+        $id = $conn->fetchColumn($qb->getSQL());
 
         if ($id) {
             $this->objectIds->attach($objectIdentity, $id);
@@ -132,7 +132,7 @@ class AccessManager implements AccessManagerInterface
             ->where($qb->expr()->eq('oi.type', $qb->expr()->literal($objectIdentity->getType())))
             ->andWhere($qb->expr()->eq('oi.identifier', $qb->expr()->literal($objectIdentity->getIdentifier())));
 
-        $entries = $conn->fetchAll($qb);
+        $entries = $conn->fetchAll($qb->getSQL());
 
         $acl = new AccessControlList($this->permissionRegistry->get($objectIdentity->getType()), $objectIdentity);
 
