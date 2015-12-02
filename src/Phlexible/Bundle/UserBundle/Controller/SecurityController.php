1a3310862a2d02c60915543e03b99fb782959a13
Peter Fahsel <pfahsel@brainbits.net>
2015-12-02 11:12:50 +0100

    fix display of login form after failed authentication

--- src/Phlexible/Bundle/UserBundle/Controller/SecurityController.php
+++ src/Phlexible/Bundle/UserBundle/Controller/SecurityController.php
@@ -23,45 +23,6 @@ use Symfony\Component\Security\Core\Security;
 class SecurityController extends BaseSecurityController
 {
     /**
-     * @param Request $request
-     *
-     * @return Response
-     * @Route("/login", name="fos_user_security_login")
-     */
-    public function loginAction(Request $request)
-    {
-        $session = $request->getSession();
-
-        $authErrorKey = Security::AUTHENTICATION_ERROR;
-        $lastUsernameKey = Security::LAST_USERNAME;
-
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has($authErrorKey)) {
-            $error = $request->attributes->get($authErrorKey);
-        } elseif (null !== $session && $session->has($authErrorKey)) {
-            $error = $session->get($authErrorKey);
-            $session->remove($authErrorKey);
-        } else {
-            $error = null;
-        }
-
-        if (!$error instanceof AuthenticationException) {
-            $error = null; // The value does not come from the security component.
-        }
-
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
-
-        $csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
-
-        return $this->renderLogin(array(
-            'last_username' => $lastUsername,
-            'error' => $error,
-            'csrf_token' => $csrfToken,
-        ));
-    }
-
-    /**
      * {@inheritdoc}
      */
     protected function renderLogin(array $data)
