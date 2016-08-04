29e7048835153ef597368836a952209865a7ff41
Tim Hoepfner <thoepfner@brainbits.net>
2016-08-04 11:21:54 +0200

    BBPHX-35 keep correct line number in console wrapper

--- src/Phlexible/Bundle/GuiBundle/Resources/scripts/util/Console.js
+++ src/Phlexible/Bundle/GuiBundle/Resources/scripts/util/Console.js
@@ -36,96 +36,49 @@ Ext.provide('Phlexible.gui.util.Console');
  * (defaults to 10)
  */
 Phlexible.gui.util.Console = function () {
+    "use strict";
     // private
+    var self = this;
     var enabled = true;
 
-    // private
-    var xcall = function (method, args) {
-        if (enabled && typeof window.console === 'object' && typeof window.console[method] === 'function') {
-            window.console[method].apply(window.console, args);
-        }
-    };
-
-    // Enable / disable loggin
-    this.enable = function () {
+    // Enable / disable logging
+    self.enable = function () {
         enabled = true;
     };
-    this.disable = function () {
+    self.disable = function () {
         enabled = false;
     };
 
-    // console wrapper
-    this.clear = function () {
-        var m = 'clear';
-        xcall(m, arguments);
-    };
-    this.log = function () {
-        var m = 'log';
-        xcall(m, arguments);
-    };
-    this.debug = function () {
-        var m = 'debug';
-        xcall(m, arguments);
-    };
-    this.info = function () {
-        var m = 'info';
-        xcall(m, arguments);
-    };
-    this.warn = function () {
-        var m = 'warn';
-        xcall(m, arguments);
-    };
-    this.error = function () {
-        var m = 'error';
-        xcall(m, arguments);
-    };
-    this.assert = function () {
-        var m = 'assert';
-        xcall(m, arguments);
-    };
-    this.dir = function () {
-        var m = 'dir';
-        xcall(m, arguments);
-    };
-    this.dirxml = function () {
-        var m = 'dirxml';
-        xcall(m, arguments);
-    };
-    this.trace = function () {
-        var m = 'trace';
-        xcall(m, arguments);
-    };
-    this.group = function () {
-        var m = 'group';
-        xcall(m, arguments);
-    };
-    this.groupCollapsed = function () {
-        var m = 'groupCollapsed';
-        xcall(m, arguments);
-    };
-    this.groupEnd = function () {
-        var m = 'groupEnd';
-        xcall(m, arguments);
-    };
-    this.time = function () {
-        var m = 'time';
-        xcall(m, arguments);
-    };
-    this.timeEnd = function () {
-        var m = 'timeEnd';
-        xcall(m, arguments);
-    };
-    this.profile = function () {
-        var m = 'profile';
-        xcall(m, arguments);
-    };
-    this.profileEnd = function () {
-        var m = 'profileEnd';
-        xcall(m, arguments);
-    };
-    this.count = function () {
-        var m = 'count';
-        xcall(m, arguments);
-    };
+    [
+        'clear',
+        'log',
+        'debug',
+        'info',
+        'warn',
+        'error',
+        'assert',
+        'dir',
+        'dirxml',
+        'trace',
+        'group',
+        'groupCollapsed',
+        'groupCollapsed',
+        'time',
+        'timeEnd',
+        'profile',
+        'profileEnd',
+        'count'
+    ].forEach(function(methodName) {
+        if (enabled && typeof window.console === 'object' && typeof window.console[methodName] === 'function') {
+            if (Function.prototype.bind) {
+                self[methodName] = Function.prototype.bind.call(window.console[methodName], window.console);
+            } else {
+                self[methodName] = function() {
+                    Function.prototype.apply.call(window.console[methodName], window.console, arguments);
+                };
+            }
+        } else {
+            self[methodName] = function() {};
+        }
+    });
 };
-
