5b732b3e54c7705f30987d0ae1a664a71d330eb4
Matthias Harmuth <mharmuth@brainbits.net>
2018-10-22 16:21:52 +0200

    Fix wrong orm mapping in entity

--- src/Phlexible/Bundle/TreeBundle/Entity/TreeNode.php
+++ src/Phlexible/Bundle/TreeBundle/Entity/TreeNode.php
@@ -42,7 +42,6 @@ class TreeNode implements TreeNodeInterface, HierarchicalDomainObjectInterface
 
     /**
      * @var int
-     * @ORM\Column(name="parent_id", type="integer", nullable=true)
      * @ORM\ManyToOne(targetEntity="TreeNode", cascade={"persist"})
      * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
      */
